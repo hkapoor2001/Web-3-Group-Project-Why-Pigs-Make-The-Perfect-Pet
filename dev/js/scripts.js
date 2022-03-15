f2a45a7f7fd4cae4f9d10d7d4e2bb7874fa65281
hkapoor2001 <hkapoor2001@gmail.com>
2022-03-15 12:38:30 -0400

    dark mode

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -29,3 +29,32 @@ window.addEventListener('load', function (){
 
 
 // test
+
+window.addEventListener('load', function (){
+  const toggleSwitch= document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    }
+
+    console.log(toggleSwitch.checked)
+    
+    e.stopPropogration();
+}
+
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if( currentTheme === 'dark'){
+  toggleSwitch.checked = true;
+}
+})
+
